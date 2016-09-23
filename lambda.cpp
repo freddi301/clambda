48ac4aa616c38a5929d6581c0eb5bffd5efc9a9b
Frederik Batuna <gobi301@gmail.com>
2016-09-23 08:28:44 -0700

    add: nested typechecking

--- lambda.cpp
+++ lambda.cpp
@@ -15,7 +15,7 @@ class Term {
 std::ostream& operator<<(std::ostream &stream, const Term &t) {
   return stream << t.pretty_print();
 }
-std::ostream& operator<<(std::ostream &stream, const Term* t) {
+std::ostream& operator<<(std::ostream &stream, const Term* const t) {
   return stream << t->pretty_print();
 }
 
@@ -28,9 +28,9 @@ class Identifier: public Term {
 
 class Application: public Term {
   public:
-    Term* const left;
-    Term* const right;
-    Application(Term* left_, Term* right_): left(left_), right(right_) {}
+    const Term* const left;
+    const Term* const right;
+    Application(const Term* const left_, const Term* const right_): left(left_), right(right_) {}
     std::string pretty_print() const {
       return "(" + left->pretty_print() + " " + right->pretty_print() + ")";
     }
@@ -38,19 +38,19 @@ class Application: public Term {
 
 class Lambda: public Term {
   public:
-    Identifier* const head;
-    Term* const body;
-    Lambda(Identifier* head_, Term* body_): head(head_), body(body_) {};
+    const Identifier* const head;
+    const Term* const body;
+    Lambda(const Identifier* const head_, const Term* const body_): head(head_), body(body_) {};
     std::string pretty_print() const {
       return "λ"+head->pretty_print()+"."+body->pretty_print();
     }
 };
 
 struct evaluated {
-  Lambda* lambda;
-  std::map<std::string, Lambda*> env;
+  const Lambda* const lambda;
+  const std::map<std::string, const Lambda*> env;
 };
-std::ostream& operator<<(std::ostream &stream, const std::map<std::string, Lambda*> &env) {
+std::ostream& operator<<(std::ostream &stream, const std::map<std::string, const Lambda*> &env) {
   stream << "{";
   for (auto t : env) { stream << " " << t.first << " = " << t.second->pretty_print() << " "; };
   stream << "}";
@@ -60,10 +60,10 @@ std::ostream& operator<<(std::ostream &stream, const struct evaluated &e) {
   return stream << e.lambda << " " << e.env;
 }
 
-const struct evaluated evaluate(Term *ast, std::map<std::string, Lambda*> env) {
-  if (Application* a = dynamic_cast<Application*>(ast)) {
-    Lambda* const left = dynamic_cast<Lambda*>(a->left);
-    Lambda* const right = dynamic_cast<Lambda*>(a->right);
+const struct evaluated evaluate(const Term* const ast, std::map<std::string, const Lambda*> env) {
+  if (const Application* a = dynamic_cast<const Application*>(ast)) {
+    const Lambda* const left = dynamic_cast<const Lambda*>(a->left);
+    const Lambda* const right = dynamic_cast<const Lambda*>(a->right);
     if (left && right) {
       env[left->head->name] = right;
       return evaluate(left->body, env);
@@ -74,17 +74,82 @@ const struct evaluated evaluate(Term *ast, std::map<std::string, Lambda*> env) {
       return evaluate(new(GC) Application(r.lambda, right), r.env);
     }
   }
-  else if (Identifier* i = dynamic_cast<Identifier*>(ast)) {
+  else if (const Identifier* i = dynamic_cast<const Identifier*>(ast)) {
     return {env.at(i->name), env};
   }
-  else if (Lambda* l = dynamic_cast<Lambda*>(ast)) { return {l, env}; }
+  else if (const Lambda* l = dynamic_cast<const Lambda*>(ast)) { return {l, env}; }
   throw std::invalid_argument( "invalid ast" );
 }
 
-Identifier* i(std::string name) { return new(GC) Identifier(name); }
-Application* a(Term* left, Term* right) { return new(GC) Application(left, right); }
-Lambda* l(Identifier* head, Term* body) { return new(GC) Lambda(head, body); }
-const struct evaluated e(Term *ast) { return evaluate(ast, std::map<std::string, Lambda*>()); }
+Identifier* i(const std::string name) { return new(GC) Identifier(name); }
+Application* a(const Term* const left, const Term* const right) { return new(GC) Application(left, right); }
+Lambda* l(const Identifier* const head, const Term* const body) { return new(GC) Lambda(head, body); }
+const struct evaluated e(const Term* const ast) { return evaluate(ast, std::map<std::string, const Lambda*>()); }
+
+class Type {
+  public:
+    virtual std::string pretty_print() const = 0;
+};
+std::ostream& operator<<(std::ostream &stream, const Type &t) {
+  return stream << t.pretty_print();
+}
+std::ostream& operator<<(std::ostream &stream, const Type* const t) {
+  return stream << t->pretty_print();
+}
+
+class TypeHolder: public Type {
+  public:
+    const int name;
+    TypeHolder(int name_): name(name_) {}
+    std::string pretty_print() const { return std::to_string(name); }
+};
+
+class LambdaType: public Type {
+  public:
+    const Type* const head;
+    const Type* const body;
+    LambdaType(const Type* const head_, const Type* const body_): head(head_), body(body_) {};
+    std::string pretty_print() const {
+      return "("+head->pretty_print()+" -> "+body->pretty_print()+")";
+    }
+};
+
+struct typechecked { const Type* const type; const int nextType; };
+std::ostream& operator<<(std::ostream &stream, std::map<std::string, const Type*> &map) {
+  stream << "{";
+  for (auto t : map) { stream << " " << t.first << " := " << t.second << " "; };
+  stream << "}";
+  return stream;
+}
+
+const struct typechecked typecheck(const Term* const ast, int nextType, std::map<std::string, const Type*> map) {
+  if (const Application* a = dynamic_cast<const Application*>(ast)) {
+    const Lambda* const left = dynamic_cast<const Lambda*>(a->left);
+    const Lambda* const right = dynamic_cast<const Lambda*>(a->right);
+    if (left && right) {
+      const struct typechecked tc = typecheck(right, nextType, map);
+      map[left->head->name] = tc.type;
+      return typecheck(left->body, tc.nextType, map);
+    } else if (left) {
+      //return evaluate(new(GC) Application(left, evaluate(a->right, env).lambda), env);
+    } else if (right) {
+      //auto r = evaluate(a->left, env);
+      //return evaluate(new(GC) Application(r.lambda, right), r.env);
+    }
+  }
+  else if (const Identifier* i = dynamic_cast<const Identifier*>(ast)) {
+    return { map.at(i->name), nextType };
+  }
+  else if (const Lambda* l = dynamic_cast<const Lambda*>(ast)) {
+    const TypeHolder* const newHolder = new (GC) TypeHolder(nextType);
+    map[l->head->name] = newHolder;
+    const struct typechecked tc = typecheck(l->body, nextType+1, map);
+    return { new (GC) LambdaType(newHolder, tc.type), tc.nextType};
+  }
+  throw std::invalid_argument( "invalid type ast" );
+}
+
+const Type* t(Term *ast) { return typecheck(ast, 1, std::map<std::string, const Type*>()).type; }
 
 int main() {
   Lambda* Identity = l(i("x"), i("x"));
@@ -98,5 +163,9 @@ int main() {
   std::cout << "False = " << False << std::endl;
   std::cout << "(a=>b=>c=>a)(I)(T)(F) = " << e(a(a(a(l(i("a"), l(i("b"), l(i("c"), i("a")))), Identity), True), False)) << std::endl;
   std::cout << "(True(Identity))(False) => " << e(a(a(True, Identity), False)) << std::endl;
+  std::cout << "True = " << True << " :: " << t(True) << std::endl;
+  std::cout << "False = " << False << " :: " << t(False) << std::endl;
+  std::cout << "(True Identity) = " << a(True, Identity) << " :: " << t(a(True, Identity)) << std::endl;
+  std::cout << "(Identity False) = " << a(Identity, False) << " :: " << t(a(Identity, False)) << std::endl;
   return 0;
 }
